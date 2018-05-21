589e3ad3d85d084981b5645817467499693b1aa1
Linmiao Xu <linmiao.xu@gmail.com>
2018-05-21 20:47:12 +0800

    Show when replays were found

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+  def replay_found_at(replay)
+    found_at = time_ago_in_words(replay.created_at)
+    if found_at =~ /minutes/
+      return "just now"
+    end
+    "#{found_at} ago".gsub(/about\s+/, '')
+  end
 end
