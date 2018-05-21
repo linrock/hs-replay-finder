5e2d4cf7546cea28487c24a3a267a2e2391637b6
Linmiao Xu <linmiao.xu@gmail.com>
2018-05-21 13:33:19 +0800

    Better ways of querying replay data

--- Gemfile
+++ Gemfile
@@ -41,6 +41,7 @@ group :development do
   # Access an interactive console on exception pages or by calling 'console' anywhere in the code.
   gem 'web-console', '>= 3.3.0'
   gem 'listen', '>= 3.0.5', '< 3.2'
+  gem 'pry'
 end
 
 group :test do
--- app/models/replay_outcome.rb
+++ app/models/replay_outcome.rb
@@ -4,27 +4,39 @@ class ReplayOutcome < ApplicationRecord
 
   # Imports replays fetched as JSON data from the API endpoint
   # https://hsreplay.net/api/v1/live/replay_feed/
-  def self.import_from_json
-    open(Rails.root.join("scripts/data/replays.746.json").to_s) do |f|
-      data = JSON.parse f.read
-      results = data["data"]
-      results.each do |replay|
-        replay_id = replay["id"]
-        next if ReplayOutcome.exists?(hsreplay_id: replay_id)
-        ReplayOutcome.create!(hsreplay_id: replay_id, data: replay)
+  def self.import_from_json(json_string)
+    data = JSON.parse json_string
+    replay_outcomes = data["data"]
+    replay_outcomes.each do |replay|
+      replay_id = replay["id"]
+      next if ReplayOutcome.exists?(hsreplay_id: replay_id)
+      ReplayOutcome.create!(hsreplay_id: replay_id, data: replay)
+    end
+  end
+
+  def self.import_from_downloaded_json
+    filenames = Dir.glob("scripts/data/replays.*.json").sort_by do |name|
+      name[/replays\.(\d*)\.json/, 1].to_i
+    end
+    filenames.each do |filename|
+      begin
+        import_from_json open(filename).read
+      rescue
+        binding.pry
       end
     end
   end
 
-  def self.find_by_criteria
+  def self.filter_by_criteria
     archetype_id = '219'
-    min_rank = 5
+    max_rank = 0
     where("
       (
         data ->> 'player1_archetype' = ? AND
         data ->> 'player2_won' = 'False' AND
         (
-          data -> 'player1_rank' < '?' OR
+          data ->> 'player1_rank' = 'None' OR
+          (data ->> 'player1_rank')::int <= '?' OR
           data ->> 'player1_legend_rank' != 'None'
         )
       ) OR
@@ -32,11 +44,12 @@ class ReplayOutcome < ApplicationRecord
         data ->> 'player2_archetype' = ? AND
         data ->> 'player2_won' = 'True' AND
         (
-          data -> 'player2_rank' < '?' OR
+          data ->> 'player2_rank' = 'None' OR
+          (data ->> 'player2_rank')::int <= '?' OR
           data ->> 'player2_legend_rank' != 'None'
         )
       )
-    ", archetype_id, min_rank, archetype_id, min_rank)
+    ", archetype_id, max_rank, archetype_id, max_rank)
   end
 
   def replay_string
@@ -57,5 +70,11 @@ class ReplayOutcome < ApplicationRecord
 
   def replay_link
     "https://hsreplay.net/replay/#{hsreplay_id}"
+  end
+
+  def print_info
+    puts replay_string
+    puts replay_link
+    puts
   end
 end
