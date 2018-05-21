52d7621ed3c9cac40015914f685b0721efc3c9d4
Linmiao Xu <linmiao.xu@gmail.com>
2018-05-22 00:47:25 +0800

    Update hexagon svg styles

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,9 +1,7 @@
 module ApplicationHelper
   def replay_found_at(replay)
     found_at = time_ago_in_words(replay.created_at)
-    if found_at =~ /minutes/
-      return "just now"
-    end
+    return "minutes ago" if found_at =~ /minute/
     "#{found_at} ago".gsub(/about\s+/, '')
   end
 end
