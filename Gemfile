be5dfc7cbd0b2c53a16f2ddae701a6d0329451da
Linmiao Xu <linmiao.xu@gmail.com>
2018-05-23 16:08:06 +0800

    Add some failure tolerance to replay outcome importer

--- Gemfile
+++ Gemfile
@@ -4,21 +4,19 @@ git_source(:github) { |repo| "https://github.com/#{repo}.git" }
 ruby '2.5.1'
 
 gem 'rails', '~> 5.2.0'
+gem 'puma', '~> 3.11'
+
 gem 'pg', '>= 0.18', '< 2.0'
 gem 'dalli', '~> 2.7'
-gem 'puma', '~> 3.11'
+
 gem 'sass-rails', '~> 5.0'
 gem 'uglifier', '>= 1.3.0'
 gem 'mini_racer', platforms: :ruby
-
-# Use ActiveStorage variant
-# gem 'mini_magick', '~> 4.8'
-
 gem 'webpacker'
-gem 'dotenv-rails'
 
 # Reduces boot times through caching; required in config/boot.rb
 gem 'bootsnap', '>= 1.1.0', require: false
+gem 'dotenv-rails'
 
 group :development do
   # Access an interactive console on exception pages or by calling 'console' anywhere in the code.
--- app/models/replay_outcome_importer.rb
+++ app/models/replay_outcome_importer.rb
@@ -2,10 +2,26 @@ require 'open-uri'
 
 class ReplayOutcomeImporter
 
+  API_ENDPOINT = "https://hsreplay.net/api/v1/live/replay_feed/"
+
   def keep_fetching
+    n_consecutive_errors = 0
     loop do
-      json_string = open("https://hsreplay.net/api/v1/live/replay_feed/").read
-      ReplayOutcome.import_from_json json_string
+      json_string = open(API_ENDPOINT).read
+      begin
+        ReplayOutcome.import_from_json json_string
+      rescue => e
+        puts "#{e.class.name}: #{e.message}"
+        puts e.backtrace
+        n_consecutive_errors += 1
+        if n_consecutive_errors > 3
+          puts "Too many failures in a row. Exiting..."
+          exit 1
+        end
+        sleep 60 ** n_consecutive_errors
+      else
+        n_consecutive_errors = 0
+      end
       sleep 60
     end
   end
