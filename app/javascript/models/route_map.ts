c912f03560d211117ac2182ab3cec7195bac883a
Linmiao Xu <linmiao.xu@gmail.com>
2018-06-02 11:16:50 +0800

    Fix sort order of class portraits

--- app/javascript/models/route_map.ts
+++ app/javascript/models/route_map.ts
@@ -4,28 +4,26 @@ interface Route {
   winrate?: string;
 }
 
+type RouteObj = {
+  [path: string]: Route
+}
+
 export default class RouteMap {
 
-  constructor(public routeMap: { [path: string]: Route }) {
+  constructor(public routeMap: RouteObj) {
     this.routeMap = routeMap
   }
 
   get classArray(): Array<[string, Route]> {
     return Object.entries(this.routeMap)
       .filter(r => !r[1].archetype)
-      .sort((a,b) => parseFloat(a[1].winrate) - parseFloat(b[1].winrate))
+      .sort((a,b) => parseFloat(b[1].winrate) - parseFloat(a[1].winrate))
   }
 
   classArchetypeRows(className): Array<[string, Route]> {
     return Object.entries(this.routeMap)
       .filter(r => r[1].class === className && r[1].archetype)
-      .sort((a,b) => parseFloat(a[1].winrate) - parseFloat(b[1].winrate))
-  }
-
-  classArchetypes(className): Array<Route> {
-    return Object.values(this.routeMap)
-      .filter(r => r.class === className && r.archetype)
-      .sort((a,b) => parseFloat(a.winrate) - parseFloat(b.winrate))
+      .sort((a,b) => parseFloat(b[1].winrate) - parseFloat(a[1].winrate))
   }
 
   getRoute(path): Route {
